75d4f6da5e3a2392bbd07103214f060038939d3c
zyl891229@hotmail.com <zyl891229@hotmail.com>
2016-04-13 23:15:14 +0800

    20160413_1

--- src/main/java/com/mrlong/inter/ChangeLetter.java
+++ src/main/java/com/mrlong/inter/ChangeLetter.java
@@ -5,7 +5,13 @@ package com.mrlong.inter;
  * 两个类实现
  * 配置到bean中
  * 
- * 通过这个案例可以初步体会到di配合接口编程的却可以解耦
+ * 通过这个案例可以初步体会到DI配合接口编程的却可以解耦
+ * 
+ * 思考题，ValidateUser接口
+ * 要给方法check（）
+ * 两个类实现
+ * CheckUser1 implement ValidateUser 按照XML验证
+ * CheckUser2 implement ValidateUser 按照数据库验证
  */
 public interface ChangeLetter {
 	public String change();
